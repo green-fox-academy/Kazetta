014d58916d3e7e95797f91eefcce01acc68c3bd2
Kazetta <tothbar28@gmail.com>
2019-12-05 13:38:16 +0100

    7exercises

---
+++ week1/Day4/hello-others.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log('Hello Bence!');
+console.log('Hello Tomi!');
+console.log('Hello Noemi!');
