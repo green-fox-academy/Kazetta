9f2eee5d7c2b5b766dc5c13f3382ddb7d302e1cd
Kazetta <tothbar28@gmail.com>
2019-12-05 13:58:08 +0100

    define-basic-info-done

---
+++ week1/Day4/define-basic-info.ts
@@ -0,0 +1,18 @@
+
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myname: string ='Barbara Toth';
+console.log(myname);
+
+let myage: number = 26;
+console.log(myage);
+
+let myheight: number = 1.67; 
+console.log(myheight + "m");
+
