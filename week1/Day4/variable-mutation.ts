bdc75a7b341ade6f0c34579e1771efc2ac027cbb
Kazetta <tothbar28@gmail.com>
2019-12-05 14:44:41 +0100

    variable-mutation-done

---
+++ week1/Day4/variable-mutation.ts
@@ -0,0 +1,53 @@
+'use strict';
+
+let a: number = 3;
+// make it bigger by 10
+console.log(a + 10);
+
+let b: number = 100;
+// make it smaller by 7
+
+console.log(b - 7);
+
+let c: number = 44;
+// double c's value
+
+console.log(c * 2);
+
+let d: number = 125;
+// divide d's value by 5
+
+console.log(d / 5);
+
+let e: number = 8;
+// what's the cube of e's value?
+
+console.log(e ** 3);
+
+let f1: number = 123;
+let f2: number = 345;
+// tell if f1 is bigger than f2 (as a boolean)
+let checkf1: boolean = f1 > f2;
+console.log(checkf1);
+
+let g1: number = 350;
+let g2: number = 200;
+// tell if the double of g2 is bigger than g1 (pras a boolean)
+let checkg2: boolean = g2 * 2 > g1;
+console.log(checkg2);
+
+let h: number = 1357988018575474;
+// tell if 11 is a divisor of h (as a boolean)
+let checkh: boolean = h% 11 == 0;
+console.log(checkh);
+
+let i1: number = 10;
+let i2: number = 3;
+// tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
+let checki: boolean = i1 > i2 ** 2 && i1 < i2 **3;
+console.log(checki);
+
+let j: number = 1521;
+// tell if j is divisible by 3 or 5 (as a boolean)
+let checkj: boolean = j% 3 == 0 || j% 5 == 0;
+console.log(checkj);
