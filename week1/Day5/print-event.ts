8499c444cc2270e327ab47b30fe4e432db227060
Kazetta <tothbar28@gmail.com>
2019-12-06 09:07:54 +0100

    print-event-done

---
+++ week1/Day5/print-event.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+
+for (let i: number = 0; i < 99; i++) {
+  console.log(i); // Prints the numbers from 0 to 99
+}
