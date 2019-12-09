4cc3972ca6146ae6936e217e0b234edb72c25d29
Kazetta <tothbar28@gmail.com>
2019-12-09 17:15:42 +0100

    swap-elements-done

---
+++ week2/day1/print-params.ts
@@ -0,0 +1,9 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+'use strict';
+
+function printParams(n1:number) {
+
+} 
