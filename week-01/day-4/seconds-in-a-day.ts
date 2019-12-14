16328f84800fccf7c350b97b467e6a163f79d3c5
Kazetta <tothbar28@gmail.com>
2019-12-14 23:50:35 +0100

    seconds-in-a-day-done

--- week-01/day-4/seconds-in-a-day.ts
+++ week-01/day-4/seconds-in-a-day.ts
@@ -7,14 +7,5 @@ let currentSeconds: number = 42;
 // Write a program that prints the remaining seconds (as an integer) from a
 // day if the current time is represented by these variables
 
-let oneday: number = 86400;
-let cs: number = 42;
-let cm: number = 60 * 34;
-let ch: number = 14 * 60 * 60;
-
-console.log(oneday - (cs + cm + ch));
-
-
-
-//42 + 2040 + 50400 = 52482;
-//86400 - 52482 = 33918;
+let remaining:number = currentHours*3600 + currentMinutes*60 + currentSeconds;
+console.log(((24*3600 - remaining) + ' seconds remaining'));
