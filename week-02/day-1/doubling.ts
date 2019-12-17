28a979c3d8aaf442e87369497b603570a5d8478c
Kazetta <tothbar28@gmail.com>
2019-12-17 22:40:53 +0100

    doubling-done

--- week-02/day-1/doubling.ts
+++ week-02/day-1/doubling.ts
@@ -1,10 +1,23 @@
+'use strict'
 // -  Create a variable named `baseNum` and assign the value `123` to it
 // -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
 // -  Log the result of `doubling(baseNum)` to the console
 
 let baseNum: number = 123;
 
-function doubling(N1:number) {
-    return N1 *2;
+function doubling(baseNum:number) {
+    return baseNum*=2
 }
+
 console.log(doubling(baseNum));
+
+/*'use strict';
+
+let baseNum: number = 123;
+
+function doubling (numberInput: number):number {
+return numberInput*2;
+
+}
+
+console.log(doubling(baseNum));*/
