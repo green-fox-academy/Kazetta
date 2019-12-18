5156965006ab9f2cc189a883483dbfdf025d1c0f
Kazetta <tothbar28@gmail.com>
2019-12-18 16:22:12 +0100

    increment-element-done

---
+++ week-02/day-1/increment-element.ts
@@ -0,0 +1,8 @@
+'use strict'
+// -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
+// -  Increment the third element simply by accessing it
+// -  Log the third element to the console
+
+let numList2:number [] = [1,2,3,4,5];
+numList2[2]++;
+console.log(numList2[2]);
