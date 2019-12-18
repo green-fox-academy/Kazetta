a4517d02f1c50d178b00c7d38959edae6a0e4ae7
Kazetta <tothbar28@gmail.com>
2019-12-18 13:58:00 +0100

    fizz-buzz-done

--- week-01/day-5/fizz-buzz.ts
+++ week-01/day-5/fizz-buzz.ts
@@ -5,17 +5,17 @@
 // and for the multiples of five print “Buzz”.
 // For numbers which are multiples of both three and five print “FizzBuzz”.
 
-
-for (let i: number = 1; i <= 100; i++) {
-
-    if (i % 3 == 0 && i % 5 !== 0) {
-        console.log('Fizz');
-    } else if (i % 3 !== 0 && i % 5 == 0) {
-        console.log('Buzz');
-    } else if (i % 3 == 0 && i % 5 == 0) {
+for (let i = 1; i <=100; i++) {
+    if (i%3===0 && i%5===0) {
         console.log('Fizzbuzz');
-    } else {
-        console.log(i)
+    } 
+    else if (i%5===0) {
+        console.log('Buzz');
     }
-
+    else if (i%3===0) {
+        console.log('Fizz');
+    }
+    else {
+        console.log(i);
+    }
 }
