865634f71b8b610bd0dc585e2ee60d9a499e1b92
Kazetta <tothbar28@gmail.com>
2020-02-02 16:55:17 +0100

    fixing some bug

--- week-06/day-03/content-2/content-2.js
+++ week-06/day-03/content-2/content-2.js
@@ -1,16 +1,17 @@
 'use strict';
 
 let listContent = ['apple', 'banana', 'cat', 'dog'];
+
 let ulElement = document.querySelector('ul');
 
 console.log(ulElement.children);
 console.log(ulElement.children[1]);
 console.log(ulElement.children[2].innerText);
 
-let ilElement = document.querySelectorAll('li');
+let ilEl = document.querySelectorAll('li');
 
 for (let i = 0; i < ilEl.length; i++) {
-    ilElement[i].innerText = listContent[i];
+    ilEl[i].innerText = listContent[i];
 }
 
 console.log(ulElement.children);
