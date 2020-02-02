0da67ecfff8108c794293eb7fd0e3dd3e0bd24d7
Kazetta <tothbar28@gmail.com>
2020-02-02 21:58:21 +0100

    go your own way - DOM practicing and playing with css

---
+++ week-06/day-03/xpractice/changingCSSStyles/changingcss.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let title = document.getElementById("page-title");
+
+title.setAttribute("style","font-size:200px; margin:0; padding:0; word-spacing: 40px; line-height: 110%; text-shadow: 6px 5px MidnightBlue;color: Gold;font-family: 'Monoton', cursive;");
+
+title.style.left = "20px";
+
+let mybody = document.getElementsByTagName("body");
+mybody.setAttribute("style","background-color: red, high: 200px;");
