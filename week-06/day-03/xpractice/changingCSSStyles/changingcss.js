a0ac01a9dec32134490565445c71339eaa05844d
Kazetta <tothbar28@gmail.com>
2020-02-03 10:08:17 +0100

    added a button to my html page

--- week-06/day-03/xpractice/changingCSSStyles/changingcss.js
+++ week-06/day-03/xpractice/changingCSSStyles/changingcss.js
@@ -1,10 +1,21 @@
 'use strict';
 
 let title = document.getElementById("page-title");
+let body = document.getElementsByTagName("body")[0];
+// let mybutton = document.getElementsByTagName("button")[0];
 
-title.setAttribute("style","font-size:200px; margin:0; padding:0; word-spacing: 40px; line-height: 110%; text-shadow: 6px 5px MidnightBlue;color: Gold;font-family: 'Monoton', cursive;");
+let mybutton = document.querySelector("div > .rigo");
 
+title.setAttribute("style","font-size:200px; margin:0; padding:0; word-spacing: 40px; line-height: 110%; text-shadow: 6px 6px MidnightBlue; color: Gold;font-family: 'Monoton', cursive;");
 title.style.left = "20px";
+title.innerHTML = "Go your own way";
 
-let mybody = document.getElementsByTagName("body");
-mybody.setAttribute("style","background-color: red, high: 200px;");
+body.setAttribute("style", "background-color: Turquoise");
+
+mybutton.innerHTML = "This button do nothing at all";
+mybutton.setAttribute("style","font-family:monserrat;border-color:MidnightBlue ;color:Turquoise; padding: 16px; margin: 0.5% ; border-radius: 12px; font-size: 24px; background-color: MidnightBlue");
+
+mybutton.onclick = () => {
+    alert('I told you');
+    console.log('after alert');
+}
