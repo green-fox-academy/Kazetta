e8259c4c0e8dc03f2398939ee396568e647c58fc
Kazetta <tothbar28@gmail.com>
2020-02-17 11:20:29 +0100

    back to school

--- week-06/day-04/todos/index.js
+++ week-06/day-04/todos/index.js
@@ -4,36 +4,83 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const todos = ['get up', 'survive', 'go back to bed'];
+const cocktails = [
+  {
+    name: 'GIN FIZZ',
+    price: 1520,
+    contains: ['gin', 'sugar', 'lemon juice', 'soda'],
+    isAlcoholic: true,
+  },
+  {
+    name: 'BLOODY MARY',
+    price: 1650,
+    contains: ['vodka', 'tomato juice', 'spices'],
+    isAlcoholic: true,
+  },
+  {
+    name: 'SEX ON THE BEACH',
+    price: 1850,
+    contains: [
+      'vodka',
+      'peach schnapps',
+      'orange juice',
+      'cranberry juice'
+    ],
+    isAlcoholic: true,
+  },
+  {
+    name: 'CUBA LIBRE',
+    price: 1850,
+    contains: ['rum', 'cola', 'lime juice'],
+    isAlcoholic: true,
+  },
+  {
+    name: 'MOJITO',
+    price: 1850,
+    contains: ['rum', 'sugar', 'lime juice', 'soda water'],
+    isAlcoholic: true,
+  },
+  {
+    name: 'LONG ISLAND ICE TEA',
+    price: 2450,
+    contains: ['vodka', 'rum', 'gin', 'tequila', 'triple sec', 'cola'],
+    isAlcoholic: true,
+  },
+  {
+    name: 'VIRGIN MOJITO',
+    price: 990,
+    contains: ['sugar', 'lime juice', 'soda water'],
+    isAlcoholic: false,
+  },
+  {
+    name: 'SAFE SEX ON THE BEACH',
+    price: 990,
+    contains: ['peach schnapps', 'orange juice', 'cranberry juice'],
+    isAlcoholic: false,
+  },
+];
+const alcoholList = ['gin', 'vodka', 'rum', 'tequila'];
 
-//set the engine to ejs
-app.set('view enjine', 'ejs');
-app.use(express.static('public'));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-//home page
+// home page
 app.get('/', (req, res) => {
-    res.sendfile(__dirname + '/index.html');
+  let nameRead = req.query.name;
+  let name;
+
+  if (nameRead) {
+    name = nameRead;
+  } else {
+    name = 'Guest';
+  }
+  // render `home.ejs`
+  res.render('home', {
+    title: `Hello`, name: name, todos: todos //left side: name in ejs, right side: name here
+  });
 });
 
-//todo page
-app.get('/todo', (req, res) => {
-
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
-
-
-//start express app on port 3000
-app.listen(PORT, function () {
-    console.log(`Listening on port ${PORT}`);
-});
-
-//database
-const database = {
-    todos: [
-        'Get up',
-        'live',
-        'love',
-        'regret',
-        'hate',
-        'no regret',
-        'Sleep'
-    ]
-};
