ba163708fce3c41e5b4c82f482c755ac3b7a28e5
Kazetta <tothbar28@gmail.com>
2020-02-17 16:08:17 +0100

    reject or not reject exercise done

---
+++ week-09/day-01/promise-it-wont-hurt/promise/reject-or-not-reject.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/*## Task
+
+- First, create a promise using the Promise constructor as we have been doing.
+- In the promise’s executor, immediately attempt to fulfill the promise with a
+value of 'I FIRED'.
+- Then, after the fulfill call, immediately try to reject the promise with an
+Error created with parameter 'I DID NOT FIRE'.
+- After the promise creation, create a function onRejected with one parameter
+error that prints the Error’s message with console.log.
+- Lastly, pass console.log and the function you just created as the success
+and rejection handlers respectively.
+
+If successful, your script should only log “I FIRED” and should not log
+“I DID NOT FIRE”.
+
+Note that unlike the prior exercises, you do not have to use setTimeout with this.*/
+
+
+function delayWithPromise() {
+    return new Promise(function (resolve, reject) {
+        resolve('I FIRED');
+        reject(new Error('I DID NOT FIRE'));
+    });
+};
+
+function onRejected (error) {
+    console.log(error.message);
+};
+
+delayWithPromise()
+    .then((result) => {
+        console.log(result);
+    },(err) => {
+    onRejected(err);
+    })
+
+// promise.then(console.log, onReject);
